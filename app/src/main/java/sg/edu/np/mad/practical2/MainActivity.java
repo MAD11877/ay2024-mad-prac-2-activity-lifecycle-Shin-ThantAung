72cb773efd96f76473edab008fa381f86e54b3fd
Shin Thant Aung <150310252+Shin-ThantAung@users.noreply.github.com>
2024-05-03 12:35:22 +0800

    Finish Practical 2

---
+++ app/src/main/java/sg/edu/np/mad/practical2/MainActivity.java
@@ -0,0 +1,40 @@
+package sg.edu.np.mad.practical2;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+import android.widget.Button;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        //Initialize a new user object
+        User user = new User("John Doe", "MAD Developer", 1, false);
+
+        //Get the TextViews and Button from the layout
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button btnFollow = findViewById(R.id.btnFollow);
+
+        //Set the TextViews with the user's name, description and default button messgae
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("Follow");
+    }
+
+}
